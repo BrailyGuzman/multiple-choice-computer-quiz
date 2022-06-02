0d991a502cab8a7aef14137de49a5e0f14b064b0
BG <94770717+BrailyGuzman@users.noreply.github.com>
2022-06-01 23:54:07 -0400

    Update quiz.py
    I have added the "score" feature to this quiz which once completed, it will print how many questions you got right out of 10.

--- quiz.py
+++ quiz.py
@@ -12,6 +12,8 @@ else:
 time.sleep(1)
 
 def quiz():
+    score = 10
+    
     question_1 = "\n1. What do you call a computer on a network that requests files from another computer?\n(A) A client\n(B) A host\n(C) A router\n(D) A web server"
     print(question_1)
     answer_1 = "The Answer is: A"
@@ -20,7 +22,8 @@ def quiz():
         print("Correct!")
     else:
         print("Incorrect! " + answer_1)
-    
+        score - 1
+        
     time.sleep(1)
 
     question_2 = "\n2. Hardware devices that are not part of the main computer system and are often added later to the system.\n(A) Peripheral\n(B) Clip art\n(C) Highlight\n(D) Execute"
@@ -31,7 +34,8 @@ def quiz():
         print("Correct!")
     else:
         print("Incorrect! "  + answer_2)
-
+        score - 1
+        
     time.sleep(1)
 
     question_3 = "\n3. The main computer that stores the files that can be sent to computers that are networked together is:\n(A) Clip art\n(B) Mother board\n(C) Peripheral\n(D) File server"
@@ -43,7 +47,8 @@ def quiz():
         print("Correct!")
     else:
         print("Incorrect! " + answer_3)
-    
+        score - 1
+        
     time.sleep(1)
 
     question_4 = "\n4. How can you catch a computer virus?\n(A) Sending e-mail messages\n(B) Using a laptop during the winter\n(C) Opening e-mail attachments\n(D) Shopping on-line"
@@ -54,7 +59,8 @@ def quiz():
         print("Correct!")
     else:
         print("Incorrect! " + answer_4)
-    
+        score - 1
+        
     time.sleep(1)
 
     question_5 = "\nGoogle (www.google.com) is a:\n(A) Search Engine\n(B) Number in Math\n(C) Directory of images\n(D) Chat service on the web"
@@ -65,7 +71,8 @@ def quiz():
         print("Correct!")
     else:
         print("Incorrect! " + answer_5)
-    
+        score - 1
+        
     time.sleep(1)
 
     question_6 = "\nWhich is not an Internet protocol?\n(A) HTTP\n(B) FTP\n(C) STP\n(D) IP"
@@ -76,7 +83,8 @@ def quiz():
         print("Correct!")
     else:
         print("Incorrect! " + answer_6)
-    
+        score - 1
+        
     time.sleep(1)
 
     question_7 = "\nWhich of the following is not a valid domain name?\n(A) www.yahoo.com\n(B) www.yahoo.co.uk\n(C) www.com.yahoo\n(D) www.yahoo.co.in"
@@ -87,7 +95,8 @@ def quiz():
         print("Correct!")
     else:
         print("Incorrect !" + answer_7)
-
+        score - 1
+        
     time.sleep(1)
 
     question_8 = "\nAOL stands for:\n(A) Arranged Outer Line\n(B) America Over LAN\n(C) Audio Over LAN\n(D) America On-line"
@@ -98,7 +107,8 @@ def quiz():
         print("Correct!")
     else:
         print("Incorrect! " + answer_8)
-    
+        score - 1
+        
     time.sleep(1)
 
     question_9 = "\nAnother name for a computer chip is:\n(A) Execute\n(B) Micro chip\n(C) Microprocessor\n(D) Select"
@@ -109,7 +119,8 @@ def quiz():
         print("Correct!")
     else:
         print("Incorrect! " + answer_9)
-
+        score - 1
+        
     time.sleep(1)
 
     question_10 = "\n10. www stands for:\n(A) World Wide Web\n(B) World Wide Wares\n(C) World Wide Wait\n(D) World Wide War"
@@ -120,8 +131,9 @@ def quiz():
         print("Correct!")
     else:
         print("Incorrect! " + answer_10)
-
-    print("You have completed this quiz!")
+        score - 1
+        
+    print(f"You Have Completed The Quiz. Your Score is {score}/10")
     time.sleep(3)
     quit()
 quiz()
